20543290f15a47637aab22c7bd6a045b4bd6ec0b
zenkaiii <wintersoul@foxmail.com>
2020-01-30 21:53:37 +0800

    前两章

--- 2/func.go
+++ 2/func.go
@@ -41,10 +41,10 @@ func pow(a,b int) int {
 	return int(math.Pow(float64(a),float64(b)))
 }
 
-func sum(numbers ...int) int{
+func sum(nums ...int) int {
 	s := 0
-	for i:= range numbers{
-		s += numbers[i]
+	for i := range nums{
+		s += i
 	}
 	return s
 }
@@ -53,14 +53,14 @@ func swap(a,b *int)  {
 	*b,*a = *a,*b
 }
 
-func main() {
-	//fmt.Println(eval(3,4,"*"))
-	q,r := div(13,3)
-	fmt.Println(q,r)
-	//fmt.Println(apply(pow,3,4))
-	//fmt.Println(sum(1,2,3,4,5))
-	//
-	//a,b := 3,4
-	//swap(&a, &b)
-	//fmt.Println(a,b)
-}
+//func main() {
+//	//fmt.Println(eval(3,4,"*"))
+//	q,r := div(13,3)
+//	fmt.Println(q,r)
+//	//fmt.Println(apply(pow,3,4))
+//	//fmt.Println(sum(1,2,3,4,5))
+//	//
+//	//a,b := 3,4
+//	//swap(&a, &b)
+//	//fmt.Println(a,b)
+//}
